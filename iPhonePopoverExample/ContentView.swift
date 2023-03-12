7a6332714a66ec5bca51913daa7f06a68f8e8182
katafuchix <katafuchix@gmail.com>
2023-03-12 13:16:18 +0900

    Initial Commit

---
+++ iPhonePopoverExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iPhonePopoverExample
+//
+//  Created by cano on 2023/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPhonePopoverExample/iPhonePopoverExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhonePopoverExampleApp.swift
+//  iPhonePopoverExample
+//
+//  Created by cano on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct iPhonePopoverExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
